ddf0969b27c32a8ca61913e1f1b6e3a2cbc26933
tanymann <tanzmannp@gmail.com>
2024-05-16 08:58:16 +0200

    Abzweig

--- Data2Check/Properties/AssemblyInfo.cs
+++ Data2Check/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Data2Check")]
+[assembly: AssemblyTitle("Data2Checker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Data2Check")]
+[assembly: AssemblyProduct("Data2Checker")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
